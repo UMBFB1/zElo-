812ac396259c412a8e469b23b36831d5971aa696
UMBFB1 <umbreongame0@gmail.com>
2024-06-07 08:10:15 -0300

    07/06/2024

--- main.js
+++ main.js
@@ -45,6 +45,81 @@ const Cuidador = db.sequelize.define('Cuidadore', {
     }
 })
 
-Cuidador.sync({force: true})
+const Responsavel = db.sequelize.define('Responsavei', {
+    nome: {
+        type: db.Sequelize.STRING,
+        allowNull: false,
+    },
+    idade: {
+        type: db.Sequelize.INTEGER,
+        allowNull: false,
+    },
+    CPF: {
+        type: db.Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: db.Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    data_nascimento: {
+        type: db.Sequelize.DATE,
+        allowNull: false,
+    },
+    telefone:  {
+        type: db.Sequelize.STRING,
+        allowNull: false,
+    },
+    preferencia_de_cuidador: {
+        type: db.Sequelize.TEXT,
+        allowNull: true
+    }
+})
 
+const Idoso = db.sequelize.define('Idoso', {
+    ome: {
+        type: db.Sequelize.STRING,
+        allowNull: false,
+    },
+    idade: {
+        type: db.Sequelize.INTEGER,
+        allowNull: false,
+    },
+    CPF: {
+        type: db.Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: db.Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    data_nascimento: {
+        type: db.Sequelize.DATE,
+        allowNull: false,
+    },
+    telefone:  {
+        type: db.Sequelize.STRING,
+        allowNull: false
+    },
+    preferencia_de_atv: {
+        type: db.Sequelize.TEXT,
+        allowNull: false,
+    },
+    condicao_de_saude: {
+        type: db.Sequelize.TEXT,
+        allowNull: false,
+    },
+    necessdades_especificas: {
+        type: db.Sequelize.TEXT,
+        allowNull: false,
+    }
+})
+
+Cuidador.sync({force: true})
+Idoso.sync({force: true})
+Responsavel.sync({force: true})
 
